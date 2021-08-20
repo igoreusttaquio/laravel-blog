00e343802ea74f80331fed3694e22b592bf62825
Igor <igoreusttaquio@gmail.com>
2021-08-20 15:25:01 +0000

    Hard code posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link rel="stylesheet" href="/blog.css">
+</head>
+<body>
+    <article>
+        <h2>My First Post</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam in, repudiandae repellendus a praesentium earum dignissimos perferendis nulla voluptatum distinctio ab. Laudantium maiores dolor vel vero cupiditate, adipisci mollitia fuga.</p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+</html>
