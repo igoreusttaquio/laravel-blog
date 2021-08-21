eec42bcfb69ab567f119a31499450ba0d335d7ad
Igor <igoreusttaquio@gmail.com>
2021-08-21 23:39:13 +0000

    Associante an post with a category.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -11,4 +12,9 @@ class Post extends Model
 
     # Specify with attributes can be mass assign.
     protected $fillable = ['title', 'excerpt', 'body'];
+
+    # Eloquent relationship.
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,6 +4,9 @@
     @foreach ($posts as $post)  
         <article>
             <h1><a href="/posts/{{$post->slug}}"><?=$post->title;?></a></h1>
+            <p>
+                <a href="#">{{$post->category->name}}</a>
+            </p>
             {{ $post->excerpt }}
         </article>
     @endforeach
