1e2c8f105e54b0d52a5c5e679c3ea7db0cfcd13a
Igor <igoreusttaquio@gmail.com>
2021-08-21 15:22:44 +0000

    Using blade diretives and create a layout file.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posts</title>
-    <link rel="stylesheet" href="/blog.css">
-</head>
-<body>
-    <?php foreach ($posts as $post): ?>
+@extends ('layout')
+
+@section('content')
+    @foreach ($posts as $post)  
         <article>
-            <h1><a href="/posts/<?=$post->slug?>"><?=$post->title;?></a></h1>
-            <?= $post->excerpt; ?>
+            <h1><a href="/posts/{{$post->slug}}"><?=$post->title;?></a></h1>
+            {{ $post->excerpt }}
         </article>
-    <?php endforeach; ?>
-</body>
-</html>
+    @endforeach
+@endsection
