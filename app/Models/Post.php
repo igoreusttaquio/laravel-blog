5f45c1f7a960c9e4448d7760b424b7f969061103
Igor <igoreusttaquio@gmail.com>
2021-08-21 22:41:34 +0000

    Change the application to use database instead static files.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,60 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
+class Post extends Model
+{
+    use HasFactory;
 
-
-    public $title;
-    public $excerpt;
-    public $slug;
-    public $date;
-    public $body;
-
-    public function __construct($title, $excerpt, $slug, $date, $body)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->slug = $slug;
-        $this->date = $date;
-        $this->body = $body;
-    }
-
-    public static function all() {
-
-        return cache()->rememberForever("posts.all", function () {
-            return collect(File::files(resource_path("posts/")))
-            ->map(function($file){
-                return YamlFrontMatter::parseFile($file);
-            })
-            ->map(function($document) {
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->slug,
-                    $document->date,
-                    $document->body
-                );
-            })->sortByDesc('date');
-        });
-    }
-
-
-    public static function find($slug) {
-        return static::all()->firstWhere('slug',$slug);
-    }
-
-    public static function findOrFail($slug){
-   
-       $post = static::find($slug);
-       //dd($posts);
-       if (! $post) {
-           throw new ModelNotFoundException();
-       }
-
-       return $post;
-    }
+    # Specify with attributes can be mass assign.
+    protected $fillable = ['title', 'excerpt', 'body'];
 }
