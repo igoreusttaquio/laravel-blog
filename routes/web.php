f5c9b1b25cbb4c00aca38059f4204bad8e365add
Igor <igoreusttaquio@gmail.com>
2021-08-22 01:19:12 +0000

    Fix N+1 Problem.

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,8 @@ use App\Models\Category;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
     return view('posts', [
-        'posts' => $posts,
+        'posts' => Post::with('category')->get(),
     ]);
 });
 
