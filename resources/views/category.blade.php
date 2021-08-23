de27ea5269f45b8443157683701afe6ca4a49ad2
Igor <igoreusttaquio@gmail.com>
2021-08-23 15:06:26 +0000

    Using components variable and extract some components.

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,11 +1,9 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     <h1>Category: {{$category->name}}</h1>
     <h2>Posts in this category</h2>
     <ul>
         @foreach($category->posts as $post)
-            <li><a href="/posts/{{$post->slug}}">{{$post->title}}</a></li>
+        <li><a href="/posts/{{$post->slug}}">{{$post->title}}</a></li>
         @endforeach
     </ul>
-@endsection
+</x-layout>
