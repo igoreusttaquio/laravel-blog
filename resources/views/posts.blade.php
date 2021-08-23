2745d16ba948e285b11e3a57391c2e6ad8b8fdac
Igor <igoreusttaquio@gmail.com>
2021-08-23 13:34:12 +0000

    Extract some views in components.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,30 @@
-@extends ('layout')
+<x-layout>
+    <!--@section('content')
+        @foreach ($posts as $post)  
+            <article>
+                <h1><a href="/posts/{{$post->slug}}"><?= $post->title; ?></a></h1>
+                <p>
+                    By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in
+                    <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+                </p>
+                {!! $post->excerpt !!}
+            </article>
+        @endforeach
+    @endsection -->
 
-@section('content')
-    @foreach ($posts as $post)  
-        <article>
-            <h1><a href="/posts/{{$post->slug}}"><?=$post->title;?></a></h1>
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in
-                <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-            </p>
-            {!! $post->excerpt !!}
-        </article>
-    @endforeach
-@endsection
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+</x-layout>
