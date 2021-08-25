8d585addf3f34509e3962ead2b366a2bf5bbde5a
Igor <igoreusttaquio@gmail.com>
2021-08-25 23:37:09 +0000

    Posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <!--@section('content')
+    @section('content')
         @foreach ($posts as $post)  
             <article>
                 <h1><a href="/posts/{{$post->slug}}"><?= $post->title; ?></a></h1>
@@ -10,7 +10,7 @@
                 {!! $post->excerpt !!}
             </article>
         @endforeach
-    @endsection -->
+    @endsection
 
     @include ('_post-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
